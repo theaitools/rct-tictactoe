781f56975fae8f4e4dc492edd539c9ad57ba387e
theaitools <theaitools@gmail.com>
2024-11-11 16:33:00 +0530

    aj

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React through Aj
+          Learn React through AjN
         </a>
       </header>
     </div>
